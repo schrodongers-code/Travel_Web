59ffbf68485917c84c80dc1d789d6e3e28d6a22e
sanjaysunil <sanjaysunil1561@gmail.com>
2026-09-21 00:10:35 +0530

    C

--- travel_agency_frontend/src/components/BookingModal.jsx
+++ travel_agency_frontend/src/components/BookingModal.jsx
@@ -40,7 +40,7 @@ export default function BookingModal({ isOpen, onClose, selectedPackage, initial
 
   const handlePayment = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/book', {
+      const response = await fetch('https://travel-web-45r8.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
